8b8e6bcd6d974f95614b46a08ac1f9b1d27bb2c3
DJ <4042405+chevrenoire@users.noreply.github.com>
2025-12-04 13:16:27 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
-import boto3
-import json
-import uuid
 import datetime
+import json
 import os
+import uuid
+
+import boto3
 
 
 def lambda_handler(event, context):
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(os.environ.get('SCHEDULED_QUEUE', default='scheduled')
+    sqs = boto3.resource("sqs")
+    queue = os.environ.get("SCHEDULED_QUEUE", default="scheduled")
 
     id = str(uuid.uuid4())
 
@@ -15,30 +16,23 @@ def lambda_handler(event, context):
         "job_class": event,
         "job_id": id,
         "provider_job_id": None,
-        "queue_name": os.environ.get('SCHEDULED_QUEUE', default='scheduled'),
+        "queue_name": os.environ.get("SCHEDULED_QUEUE", default="scheduled"),
         "priority": None,
         "arguments": [],
         "executions": 0,
         "exception_executions": {},
         "locale": "en",
         "timezone": "UTC",
-        "enqueued_at": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        "enqueued_at": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
     }
 
     message_attributes = {
         "shoryuken_class": {
             "DataType": "String",
-            "StringValue": "ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper"
+            "StringValue": "ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper",
         }
     }
 
     queue.send_message(
-        MessageBody=json.dumps(message_body),
-        MessageAttributes=message_attributes
+        MessageBody=json.dumps(message_body), MessageAttributes=message_attributes
     )
-
-# if __name__ == "__main__":
-#   event = {
-#     "task_name": "test"
-#   }
-#   lambda_handler(event, None)
